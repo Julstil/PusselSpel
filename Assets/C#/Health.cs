298ff79b36b903f73adab0662e75bd899487d0db
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-11-25 15:22:12 +0100

    Health
    Kod till ett healthsystem.
    
    Tar bort hp om man rör vid ett objekt med en "Enemy" tag, startar om scenen när hp når 0 och lägger till hp vid kontakt av ett objekt med en "Health" tag, förstör även objektet med "Health" tagen.
    
    Sätts på spelaren.

---
+++ Assets/C#/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public float hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Startar om scenen när man har 0 hp. - Daniel
+        }
+        if (hp > 3)
+        {
+            hp--; //Tar bort 1 hp om man har över 3. På så vis kan man ha 3 hp som mest. - Daniel
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            hp--; //Tar bort 1 hp om man gör kontakt med ett objekt med en "Enemy" tag. - Daniel
+        }
+
+        if (collision.transform.tag == "Health")
+        {
+            hp++; //Lägger till 1 hp om man gör kontakt med ett objekt med en "Health" tag. - Daniel
+            Destroy(collision.gameObject); //Förstör objekt som har en "Health" tag när spelaren gör kontakt med den. På så sätt kan man inte få hur mycket hp som helst från en collectable. - Daniel
+        }
+    }
+}

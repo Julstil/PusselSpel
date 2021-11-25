6ec559a2e7b7891ff82864db7afa0a346dba0029
Julstil <edvinnordangard@hotmail.com>
2021-11-25 15:21:10 +0100

    evilField

---
+++ Assets/C#/evilFeild.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evilFeild : MonoBehaviour
+{
+    public GameObject red;
+    public GameObject blue;
+
+    [SerializeField]
+    string[] acceptedTag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (string item in acceptedTag)
+        {
+            if (collision.tag == item)
+            {
+                
+            }
+        }
+        
+    }
+}

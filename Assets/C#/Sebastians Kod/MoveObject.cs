1f3deb36a826ef5c6bc0f04e6567f8ab29ff3797
NeuGemi <sebastian1.lundberg@gmail.com>
2021-12-02 14:11:45 +0100

    vete fan

--- Assets/C#/Sebastians Kod/MoveObject.cs
+++ Assets/C#/Sebastians Kod/MoveObject.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObject : MonoBehaviour, Imovable
+public class MoveObject : MonoBehaviour
 {
 
-    public float speed = 0.5f;
+    public float speed = 1.0f;
 
-    private Transform target;
+    public Vector3 target;
 
-   public void moveObject()
-   {
-        //Vector3.MoveTowards
-   }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Lever")
+        {
+            Move();
+        }
+    }
 
-}
+    private void Move()
+    {
 
-public interface Imovable
-{
-    void moveObject();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 directionToMove = target - transform.position;
+            directionToMove = directionToMove.normalized * Time.deltaTime * speed;
+            float maxDistance = Vector3.Distance(transform.position, target);
+            transform.position = transform.position + Vector3.ClampMagnitude(directionToMove, maxDistance);
+        }
+    }
 }

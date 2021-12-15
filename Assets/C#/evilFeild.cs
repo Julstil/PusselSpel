f95938b5c81d65444f9ac2ec8c112ac003a8cc99
Julstil <edvinnordangard@hotmail.com>
2021-12-15 12:11:30 +0100

    bob och kommentarer

--- Assets/C#/evilFeild.cs
+++ Assets/C#/evilFeild.cs
@@ -10,28 +10,36 @@ public class evilFeild : MonoBehaviour
     string[] notAcceptedTag;
 
     [Range(5, 2000)]
-    public int pushBackX;
+    public int pushBackX = 93;
     [Range(5, 2000)]
-    public int pushBackY;
+    public int pushBackY = 61;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        foreach (string item in notAcceptedTag)
+        foreach (string item in notAcceptedTag) //För varje objekt som har en tag som är lika dan som en i notAcceptedTag arayn kommer detta hända vid kollision - Edvin
         {
-            if (collision.tag == item)
+            if (collision.tag == item) //Kollar om tagen är samma som en i notAcceptedTag - Edvin
             {
-                var playerPM = collision.GetComponent<PlayerMovement>();
-                playerPM.isActive = false;
-                collision.GetComponent<Health>().hp--;
-                var playerRB = collision.GetComponent<Rigidbody2D>();
-                if (playerRB.velocity.y < 1)
+                var playerPM = collision.GetComponent<PlayerMovement>(); // skapar en variabel för objektets/spelarens PlayerMovement kod - Edvin
+                playerPM.isActive = false; // Gör boolen isActive till falskt - Edvin
+                collision.GetComponent<Health>().hp--; // Refererar till spelarens Health kod och säger att hp variabeln ska minska med 1 - Edvin
+                var playerRB = collision.GetComponent<Rigidbody2D>(); // skapar en variabel för objektets/spelarens RigidBody - Edvin
+                if (playerRB.velocity.y < 1) // Kollar ifall objektets/spelarens hastighet är mindre än 1 - Edvin 
                 {
-                    playerRB.velocity = new Vector2(playerRB.velocity.x, playerRB.velocity.y + 1);
+                    playerRB.velocity = new Vector2(playerRB.velocity.x, playerRB.velocity.y + 1); // i så fall plussar vi på 1 i y led - Edvin
+                    /* Fixar en bug då addForce nedanför inte ville funka på ett smooth och bra sätt då den innan hackade tillbaka.
+                     * Ger inte objektet/spelaren en bost upp med pushbackY som man kanske kan tro, 1 är ganska litet och blir riktat nedåt då vi
+                     * vänder på hastigheten i nästa kod. - Edvin
+                     */
                 }
+
                 playerRB.AddForce(new Vector2(-playerRB.velocity.x * pushBackX, -playerRB.velocity.y * pushBackY));
-                playerPM.Reenable();
+                /* Vänder på objektets/spelarens hastighet och knuffar tillbaka objektet/spelaren med AddForce genom att multiplicera den nuvarande
+                 * hastigheten med variablarna pushBackX och pushBackY. - Edvin
+                 */
+
+                playerPM.Reenable(); // Sätter igång Reenable funktionen i PlayerMovement koden - Edvin
             }
         }
-        
     }
 }
